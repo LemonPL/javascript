7d82e695f1d1675c9ebfb498c54f2a1c501a6c52
LemonPL <piotr.cytryniak@gmail.com>
2017-10-20 10:04:27 +0200

    Create simpleGuessingGame.js

---
+++ simpleGuessingGame.js
@@ -0,0 +1,14 @@
+var secretNumber = 7;
+
+var guessNumber = Number(prompt("Guess the number?"));
+
+if (guessNumber < secretNumber ) {
+	alert("Too low. Guess again!");
+}
+else if (guessNumber == secretNumber) {
+	alert("You are correct!");
+}
+else{alert("Too high. Guess again!")};
+
+// typeof guessNumber -> tells us about the data type of guessNumber
+// Number(guessNumber) -> converting from string to number data type
