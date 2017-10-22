2737f6a1a1d32c5fe1d520a597500667ff665544
LemonPL <piotr.cytryniak@gmail.com>
2017-10-22 23:55:40 +0200

    Update moviesDBObjects.js

--- moviesDBObjects.js
+++ moviesDBObjects.js
@@ -28,3 +28,35 @@ var movies = [
 			console.log("I have not watched " + "\"" + movies[i].title+ "\"" + " - " + movies[i].rating + " stars");
 		}
 	}
+
+// OR
+
+	movies.forEach(function(movie){
+		var result = "You have ";
+		if (movie.hasWatched) {
+			result += "watched ";
+		}else{
+			result += "not seen ";
+		}
+		result += "\"" + movie.title + "\" - ";
+		result += movie.rating + " stars";
+		console.log(result);
+	})
+
+// optimized
+
+	function buildString(movie){
+		var result = "You have ";
+		if (movie.hasWatched) {
+			result += "watched ";
+		}else{
+			result += "not seen ";
+		}
+		result += "\"" + movie.title + "\" - ";
+		result += movie.rating + " stars";
+		return result;
+	}
+
+	movies.forEach(function(movie){
+		console.log(buildString(movie));
+	})
