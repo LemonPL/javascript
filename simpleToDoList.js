8fd76c06edf5f685493fdb165f35a173204ffd81
LemonPL <piotr.cytryniak@gmail.com>
2017-10-22 20:44:26 +0200

    delete + refactorization

--- simpleToDoList.js
+++ simpleToDoList.js
@@ -3,15 +3,34 @@ var todos = [];
 var input = prompt("What would you like to do?");
 
 while (input !== "quit"){
-
-
 	if (input === "list") {
-		console.log(todos);
+		listTodos();
 	}
 	else if (input === "new") {
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
+		newTodos();
 	}
-	input = prompt("What would you like to do?")
+	else if(input === "delete"){
+		deleteTodos();
+	}
+	input = prompt("What would you like to do?");
 }
 console.log("OK, you quit the app");
+
+function listTodos(){
+			console.log("**********");
+		todos.forEach(function(todo, i){
+			console.log(i + ": " + todo);	
+		});
+		console.log("**********")	;
+}
+
+function newTodos(){
+		var newTodo = prompt("Enter new todo");
+		todos.push(newTodo);
+		console.log("Added todo");}
+
+function deleteTodos(){
+		var index = prompt("Enter index of todo to delete");
+		todos.splice(index,1);
+		console.log("Deleted todo");
+}
